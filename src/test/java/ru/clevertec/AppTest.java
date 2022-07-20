7d942c92e7eea556d4b5a8c583847de95dec329f
Korolenko Sergey <pavilion1900@gmail.com>
2022-07-20 15:39:28 +0300

    initial commit

---
+++ src/test/java/ru/clevertec/AppTest.java
@@ -0,0 +1,11 @@
+package ru.clevertec;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test public void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+}
